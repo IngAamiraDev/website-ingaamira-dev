fa1fcf0489c021568ca1046e1b656084258ccaad
ingaamira <andres.mira@outlook.com>
2023-12-06 10:57:21 -0500

    Add navbar

--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './../../../shared/components/header/header.component';
+import { NavbarComponent } from './../../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [RouterModule, HeaderComponent, NavbarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
