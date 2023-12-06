ee6ecf53fb4534316268bae02e5fd6ca727c9f0d
ingaamira <andres.mira@outlook.com>
2023-12-06 09:50:23 -0500

    Add about, not-found, footer, header and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutComponent } from './domains/info/pages/about/about.component';
+import { LayoutComponent } from './domains/shared/components/layout/layout.component';
+import { NotFoundComponent  } from './domains/info/pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  {
+      path: '',
+      component: LayoutComponent,
+      children: [
+        {
+            path: '',
+            component: AboutComponent
+        },
+        {
+            path: 'about',
+            component: AboutComponent
+        },
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
