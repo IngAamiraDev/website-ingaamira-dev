3ba3ec488963fb70508346a5f6b675bf536d1fbd
ingaamira <andres.mira@outlook.com>
2023-12-07 13:17:53 -0500

    Add particle-background

--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './../../../shared/components/header/header.component';
 import { NavbarComponent } from './../../../shared/components/navbar/navbar.component';
 import { FooterComponent } from './../../../shared/components/footer/footer.component';
+import { ParticleBackgroundComponent } from './../../../shared/components/particle-background/particle-background.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, NavbarComponent, FooterComponent],
+  imports: [RouterModule, HeaderComponent, NavbarComponent, FooterComponent, ParticleBackgroundComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
---
+++ src/app/domains/shared/modules/modules.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ParticleBackgroundComponent } from '../components/particle-background/particle-background.component';
+
+@NgModule({
+  declarations: [
+    ParticleBackgroundComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class ModuleModule { }
