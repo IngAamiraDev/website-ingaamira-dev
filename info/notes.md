44297505ed932cf64525189f503c56cae130551d
ingaamira <andres.mira@outlook.com>
2023-12-07 06:07:21 -0500

    Update layout and navbar

--- info/notes.md
+++ info/notes.md
@@ -1,7 +1,16 @@
 # Portfolio in Angular
 
 
+## Comandos Utilizados
+npm install typed.js
+npm install @types/typed.js
+
 
 ## Recursos Adicionales
 - [ngx-bootstrap](https://valor-software.com/ngx-bootstrap/#/documentation)
 - [Creating an Angular Portfolio Website](https://www.youtube.com/playlist?list=PLN0Th-4WgKrUVQlqa14mUDeymTW1luznW)
+- [Portfolio Template](https://github.com/StyvenSoft/portfolio-template)
+- []()
+
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ingaamira&layout=compact&show_icons=true&text_color=ffffff&bg_color=0d1117)](https://github.com/ingaamira?tab=repositories)
--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './../../../shared/components/header/header.component';
 import { NavbarComponent } from './../../../shared/components/navbar/navbar.component';
+import { AboutComponent } from  './../../../info/pages/about/about.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, NavbarComponent],
+  imports: [RouterModule, HeaderComponent, NavbarComponent, AboutComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
