d32d7a88e58da052e06cd1b12922930e767b63a8
ingaamira <andres.mira@outlook.com>
2023-12-12 12:00:19 -0500

    Update home. Delete about

--- src/app/domains/info/pages/home/home.component.ts
+++ src/app/domains/info/pages/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { AboutComponent } from './../../../info/pages/about/about.component';
 import { ProjectsService } from '../../../shared/services/projects.service';
 import { Project } from '../../../shared/models/project';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
