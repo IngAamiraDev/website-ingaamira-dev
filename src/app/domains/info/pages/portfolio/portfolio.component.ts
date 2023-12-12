d7f2d25eaa5c10c1d70b51f0f8e08048b38fbadf
ingaamira <andres.mira@outlook.com>
2023-12-12 10:20:51 -0500

    Update config ngx-bootstrap

--- src/app/domains/info/pages/portfolio/portfolio.component.ts
+++ src/app/domains/info/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { ProjectCardComponent } from '../project-card/project-card.component';
 import { Project } from '../../../shared/models/project';
@@ -7,21 +8,24 @@ import { Tag } from '../../../shared/models/tag';
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
 
-  project: Project = {
-    id: 0,
-    name: 'Sample Angular App',
-    summary: 'Test Description',
-    description: '',
-    projectLink: '',
-    pictures: [],
-    tags: [Tag.ANGULAR, Tag.TYPESCRIPT]
-  };
+  projects: Project[] = [
+
+    { id: 0,
+      name: 'E-ShopApp',
+      summary: 'E-ShopApp es una aplicación de comercio electrónico desarrollada en Java con el framework Spring Boot. La aplicación permite a los usuarios explorar y comprar productos en línea, organizados en categorías.',
+      description: '',
+      projectLink: 'https://github.com/IngAamira/E-ShopApp',
+      pictures: [],
+      tags: [Tag.JAVA, Tag.SPRING]
+    }
+
+  ];
 
   constructor (private titleService: Title) {
     this.titleService.setTitle('Portfolio')
--- src/app/domains/shared/models/tag.ts
+++ src/app/domains/shared/models/tag.ts
@@ -1,11 +1,16 @@
 export class Tag {
 
+  /*Backend languages*/
   static readonly JAVA = new Tag('Java', 'orange');
+  static readonly SPRING = new Tag('Spring Boot', 'green');
   static readonly PYTHON = new Tag('Python', 'pink');
+  static readonly NODEJS = new Tag('Node.JS','green');
+
+  /*Frontend languages*/
   static readonly TYPESCRIPT = new Tag( 'TypeScript', 'blue');
   static readonly JAVASCRIPT = new Tag('JavaScript','yellow');
   static readonly ANGULAR = new Tag('Angular','red');
-  static readonly NODEJS = new Tag('Node.JS','green');
+
 
   private constructor(private readonly key: string, public readonly color: string) {
 
