f5439259c756331af4d71991b7cad7b73004aafd
ingaamira <andres.mira@outlook.com>
2023-12-19 11:29:15 -0500

    Update translate in header

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+import { TranslateService, TranslateModule} from '@ngx-translate/core';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
+  changeLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
 
 }
