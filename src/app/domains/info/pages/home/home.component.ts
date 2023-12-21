763768bcce60e9b8693a64a4a3f373033a65068c
ingaamira <andres.mira@outlook.com>
2023-12-21 14:39:03 -0500

    Update translate in home

--- src/app/domains/info/pages/home/home.component.ts
+++ src/app/domains/info/pages/home/home.component.ts
@@ -21,7 +21,12 @@ import { TranslationService } from '../../../shared/services/translation.service
 })
 export class HomeComponent implements OnInit{
 
-  featureProject = {} as Project;
+  featureProject: Project = {} as Project;
+  years: number;
+
+  private startDate: Date = new Date(2011, 11, 28);
+  //En JavaScript, los meses se indexan desde 0 (enero es 0, febrero es 1, etc.).
+  //Fecha de incio de labores (28 de diciembre del 2011).
 
   constructor (
     private titleService: Title,
@@ -29,7 +34,8 @@ export class HomeComponent implements OnInit{
     public bsModalRef: BsModalRef,
     private translationService: TranslationService
     ) {
-    this.titleService.setTitle('Home')
+    this.titleService.setTitle('Home');
+    this.years = this.calculateYears(new Date(this.startDate));
   }
   ngOnInit(): void {
     this.featureProject = this.projectService.GetProjectById(0);
@@ -37,6 +43,13 @@ export class HomeComponent implements OnInit{
 
   changeLanguage(lang: string): void {
     this.translationService.changeLanguage(lang);
+  }
+
+  private calculateYears(startDate: Date): number {
+    const currentDate = new Date();
+    const timeDiff = Math.abs(currentDate.getTime() - startDate.getTime());
+    const years = Math.floor(timeDiff / (1000 * 3600 * 24 * 365.25));
+    return years;
   }
 
 }
--- src/app/domains/shared/services/translation.service.ts
+++ src/app/domains/shared/services/translation.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
+  private currentLanguageSubject: BehaviorSubject<string> = new BehaviorSubject<string>('en');
+  currentLanguage$ = this.currentLanguageSubject.asObservable();
 
   constructor(private translate: TranslateService) {
     this.initializeTranslation();
@@ -16,7 +19,16 @@ export class TranslationService {
   }
 
   changeLanguage(lang: string): void {
-    this.translate.use(lang);
+    try {
+      this.translate.use(lang);
+      this.currentLanguageSubject.next(lang);
+    } catch (error) {
+      console.error('Error changing language:', error);
+    }
+  }
+
+  getInstantTranslation(key: string): string {
+    return this.translate.instant(key);
   }
 
 }
