b70c3cd7129f8bb9632fa82ae1fa9c2f2281d241
ingaamira <andres.mira@outlook.com>
2023-12-27 05:22:08 -0500

    Update routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    component: NotFoundComponent, pathMatch: 'full', redirectTo: 'home'
+    component: NotFoundComponent, pathMatch: 'full', redirectTo: ''
   }
 ];
 
