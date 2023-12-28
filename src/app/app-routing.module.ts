4169cce24e89a9466f84317df07dfbd8a3b90a31
ingaamira <andres.mira@outlook.com>
2023-12-28 06:44:28 -0500

    Update routing. Add translate in Name. Update CV Data en. Update index app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LayoutComponent } from './domains/shared/components/layout/layout.component';
-//import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
+import { ContactComponent } from './domains/info/pages/contact/contact.component';
 import { HomeComponent } from './domains/info/pages/home/home.component';
+import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 import { PortfolioComponent } from './domains/info/pages/portfolio/portfolio.component';
 import { ResumeComponent } from './domains/info/pages/resume/resume.component';
-import { ContactComponent } from './domains/info/pages/contact/contact.component';
+import { LayoutComponent } from './domains/shared/components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
-      {
-        path: '',
-        component: HomeComponent
-      },
-      {
-          path: 'portfolio',
-          component: PortfolioComponent
-      },
-      {
-        path: 'resume',
-        component: ResumeComponent
-      },
-      {
-        path: 'contact',
-        component: ContactComponent
-      },
-  ]
+      { path: '', component: HomeComponent },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'resume', component: ResumeComponent },
+      { path: 'contact', component: ContactComponent },
+    ]
   },
-  {
-    path: '**', pathMatch: 'full', redirectTo: ''
-  }
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot( routes, {useHash: true} ) ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/domains/info/pages/portfolio/portfolio.component.ts
+++ src/app/domains/info/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
